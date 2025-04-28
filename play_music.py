0ddd46710449e93ff5bf3057ba0d2e2257726f78
miyakp098 <ybtkeppen813@gmail.com>
2025-04-28 15:10:00 +0900

    リファクタ

--- play_music.py
+++ play_music.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Optional
 
 import discord
 import yt_dlp as youtube_dl
@@ -19,15 +20,11 @@ ytdl_format_options = {
     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
-ffmpeg_options = {
-    'options': '-vn'
-}
-
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
 class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source: discord.AudioSource, *, data: dict, volume: float = 1.0):
+    def __init__(self, source: discord.AudioSource, data: dict, volume: float = 0.5):
         """
         YTDLSourceクラスの初期化。
 
@@ -42,26 +39,42 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.url = data.get('url')
 
     @classmethod
-    async def from_url(cls, url: str, *, loop: asyncio.AbstractEventLoop | None = None, stream: bool = False) -> 'YTDLSource':
+    async def from_url(cls, url: str, *, stream: bool = False) -> 'YTDLSource':
         """
         指定されたURLから音声ソースを生成します。
 
         Args:
             url (str): YouTubeのURL。
-            loop (asyncio.AbstractEventLoop, optional): イベントループ。デフォルトはNone。
             stream (bool, optional): ストリーミングモードかどうか。デフォルトはFalse。
 
         Returns:
             YTDLSource: 生成された音声ソース。
         """
-        loop = loop or asyncio.get_event_loop()
+        data = await cls._extract_info(url, stream)
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        youtube_dl_source = cls(discord.FFmpegPCMAudio(filename, options='-vn'), data=data)
+        return youtube_dl_source
+
+    @staticmethod
+    async def _extract_info(url: str, stream: bool) -> dict:
+        """
+        YouTubeから情報を抽出します。
+
+        Args:
+            url (str): YouTubeのURL。
+            stream (bool): ストリーミングモードかどうか。
+
+        Returns:
+            dict: 抽出された情報。
+        """
+        loop = asyncio.get_running_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
         if 'entries' in data:
             data = data['entries'][0]
 
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+        return data
 
 
 
